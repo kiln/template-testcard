66c68077b784b9a77072d34a1b3c0b8ad1905704
Joe Turner <joe@oampo.co.uk>
2021-10-01 15:43:00 +0100

    Display values of the data tables

--- template.js
+++ template.js
@@ -2,10 +2,45 @@ const data = {};
 
 const state = {};
 
+function updateDataTables() {
+	for (const [name, dataset] of Object.entries(data)) {
+		const {column_names} = dataset;
+		const table = document.createElement("table");
+		const table_head = document.createElement("tr");
+		for (const columns of Object.values(column_names)) {
+				for (const column of [columns].flat()) {
+					const cell = document.createElement("th");
+					cell.textContent = column;
+					table_head.appendChild(cell);
+				}
+		}
+		table.appendChild(table_head);
+
+		for (const row of dataset) {
+			const table_row = document.createElement("tr");
+			for (const column_name of Object.keys(column_names)) {
+				const values = row[column_name];
+				for (const value of [values].flat()) {
+					const cell = document.createElement("td");
+					cell.textContent = value;
+					table_row.appendChild(cell);
+				}
+			}
+			table.appendChild(table_row);
+		}
+
+		document
+			.querySelector(`.table-container-${name.replaceAll("_", "-")}`)
+			.replaceChildren(table);
+	}
+}
+
 function draw() {
+	update();
 }
 
 function update() {
+	updateDataTables();
 }
 
 window.template = {
